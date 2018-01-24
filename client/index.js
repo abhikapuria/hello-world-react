e20552b9a5e3de638f190a4981a8b03446446912
Abhishek Kapuria <abhikapuria@gmail.com>
2018-01-23 21:18:19 -0600

    Basic React + Webpack

---
+++ client/index.js
@@ -0,0 +1,9 @@
+/**
+ * Webpack entry file
+ */
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
